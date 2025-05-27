d60c1d9b115b4b0d10551c4ee456d194cd24ec5e
Katerina Skroumpelou <sk.katherine@gmail.com>
2025-05-27 12:22:18 +0300

    added maints

--- apps/mcp-server/src/main.ts
+++ apps/mcp-server/src/main.ts
@@ -1,1 +1,8 @@
-console.log('Hello World');
+import { SpellcastingSDK } from '@astra-arcana/spellcasting-sdk';
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+
+const server = new McpServer({
+  name: 'Astra Arcana',
+  version: '1.0.0',
+});
